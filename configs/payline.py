cf1db95b3fab1501889053d3ef1978522f4a2403
patrick cieplak <patrick@finixpayments.com>
2017-01-30 10:45:07 +0000

    use correct name for maven artifact

--- configs/payline.py
+++ configs/payline.py
@@ -25,7 +25,7 @@ partner_configs = {
         'ruby_client_resource_name': "Payline",
         'ruby_gem': "payline-data",
         'ruby_require_statement': "payline",
-        'java_artifact_id': "payline-data",
+        'java_artifact_id': "payline-java",
         }   
 
 
